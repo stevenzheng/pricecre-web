8537576aa6e9f5eccb4fdfd5cb546d9ee2b24a4c
Steven Zheng <steven@pricecre.com>
2026-06-04 20:41:21 +0800

    fix: syntax error in PropertyCard indicator cell

--- components/PropertyCard.tsx
+++ components/PropertyCard.tsx
@@ -592,7 +592,6 @@ export default function PropertyCard({
                     </div>
                   </div>
                 );
-                );
               })}
             </div>
           </div>
